77b2abf6e7333d294e57e8777f8b04aa10babf55
dhyeykhanpara21 <dhyey2112004@gmail.com>
2024-02-25 14:41:25 +0530

    commit

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { Button } from "./Button";
-export { CheckBox } from "./CheckBox";
-export { Img } from "./Img";
-export { Input } from "./Input";
-export { Line } from "./Line";
-export { List } from "./List";
-export { ReactTable } from "./Table";
-export { Text } from "./Text";
